e7a82df37e983fd513dbd85f92c1b10964658a73
jkuca <46653059+jkuca@users.noreply.github.com>
2023-02-07 15:09:06 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.example.Vehicle.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Trolleybus trolleybus1 = new Trolleybus(1, 123, 30, Status.IN_OPERATION, LocalDate.parse("2022-01-01"));
+        Bus bus1 = new Bus(1, 1212, 50, Status.NEED_REPAIR, LocalDate.parse("2023-01-12"), 12);
+        Tram tram = new Tram(1, 456, 40, Status.IN_OPERATION, 21 ,LocalDate.parse("2022-01-01"));
+
+        List<Vehicle> allVehicles = new ArrayList<Vehicle>();
+
+        allVehicles.add(bus1);
+        allVehicles.add(tram);
+        allVehicles.add(trolleybus1);
+
+
+
+
     }
 }
